3eacfcdb2eea965cf3882af0a06bbf2ba4f7d94a
amankrokx <amankumar.spj410@gmail.com>
2022-04-04 13:05:43 +0530

    serviceworker added

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+ // version development 1.8.10 oyea oyea
+ // ALMOST WROKING oh yeaysvgadjkfvgvgh
+ //new update
+ self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('rnsHalls').then(function(cache) {
+            return cache.addAll([
+                './android-chrome-192x192.png',
+                './apple-touch-icon.png',
+                './browserconfig.xml',
+                './favicon-16x16.png',
+                './favicon-32x32.png',
+                './favicon.ico',
+                './mstile-144x144.png',
+                './mstile-70x70.png',
+                './safari-pinned-tab.svg',
+                './site.webmanifest',
+                './assets/material-icons.css',
+                './assets/material-icons.woff2',
+                './assets/Roboto-Regular.ttf',
+                './media/fb.png',
+                './media/google.png',
+                './media/twitter.png',
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+
+self.addEventListener('fetch', function(e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
